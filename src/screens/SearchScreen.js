4f42d8eb2c0763f9763a130bb5e9c88101c617a1
donnymboma01 <donnymboma01@gmail.com>
2023-04-07 00:33:31 +0200

    feat: search bar screen

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Text, StyleSheet, View } from "react-native";
+
+import SearchBar from "../components/SearchBar";
+
+const SearchScreen = () => {
+  return (
+    <View>
+      <SearchBar />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default SearchScreen;
